ab1ad8d034311aeaf87a84d9e9d4800f0991ff41
berkakahs <berkakahs@gmail.com>
2025-06-15 23:08:44 +0300

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,12 @@
+let fixedBody = {
+  response: "u1PI9B/Y6/z57GWlnUYjRat6ZzL149+2zbgyTlRvvCwyiAUu0RiQ88+ixulk15BvzU5xRi9Fkt8Ky6VvbobRAdm7b2YL4KjMbpA5b4D++mE/QIbe3/LZhbhF8aQH1wjzbOPWhRwcDW4AiqWnTrTAOs40E4D4E4g5Yxes1UP5CI6hg7uitS0khAWpk32feaU2rPYGwIHY27Nyy4F9EdX7SMFR+WTpGP7yr/x/EXRCgmNvhlCZRpzpRMDjhHxu4jwEb4ZcxvoAIdks0A2IvEu526RhiX2iNxj5yXoBKiEpEJ1YmDdu0ZDGOW6k/Brfgsq+tpCvPC8GO8d8pPoSzogBX83QLzVSzOqTte3HxgVI8zQugP/yw7qadiGGpzjvfjX3o6KkknOkA5n09QT00hup9euEVKGoClIjmJhaL823Kx9C8LnQaiuHnuHG/iFMP75rmeNJzf5cC6yKhR3YSIikEfcU/Jh4JkhBbd47fI8U4n5aTfULbpHhQxTQePybvbD4eKcUHjPxmr1Z5znbClWrIFUxBtWD41YL+JFGXiaPOi+yaUhXCCa/Yc5qtod0AqMqQb1p5g8givX5LKYDOmmnV1dSHvhSVgyAb32JwkAdysZf1KPquws2c18HxQ8Rmfr8Vz76ZVcstpXHcQJzBAqvC1W/8Ph3tinsPK+DJpz0xXZgec7OG5uYzxdgir8PhOPNducjlcelsjKwBikQ6UtFme3mUSGoJAQcBEXOnRE56NSw52iYqGBdR1j/cuu2PxbZxJZUGG21gEVX79eaywwN0TFuKExOxWiZeREy7XyG4CtflUvSEG574+eIIRZE2Lg/AC1T2EK1BJ3lcnnLlHyl8F+T3QYAQX2fWR0BY526rlQcb6+4x2KYjvacqxjL3o0tRgjvoXZfA9F4+VwptCDD/u8ERSygSYjy6AbY+aAE7MNhPxuJ9bEWopRkrv+JnM9oFDd6nrzwx4trfr6IN4+/yA=="
+};
+
+$done({
+  body: JSON.stringify(fixedBody),
+  headers: {
+    "token": "XovhnDGZ5GuXUaNwNPTewA==",
+    "s": "VWzs7h/A6Eh2yPnb3ePxLPgZxs7WQ3YgOESZRw4+6ECmNxH9alTQ6zY4YqRhj+mun5IhIDrAYoJnSrmPUz8NFHFu/BfNOKPAu/7plgveWSqneZVWwLI8hfU45rbj6wkN1znGy8TbfBRkRUCcNIUfkXXmZmJ89ZFj/pNs//kXxVE=",
+    "etag": 'W/"467-7YfHeUMRGzDN0SGo18+nGeAccCc"'
+  }
+});
