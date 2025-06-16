43fb501b1af7e212259b4349f0da3c2d6f654212
berkakahs <berkakahs@gmail.com>
2025-06-16 23:31:42 +0300

    Create calisartik2.js

---
+++ calisartik2.js
@@ -0,0 +1,4 @@
+let body = {
+  request: "y1KGTV56b2nPKvP6c1dVzvrvaawsb4b4\/EMnJQYLqY8+pSKPiVguvsl\/DnwK85UO+u9\/rWrn1vFwnLWPPDUDg\/HNuy5ENF1ZoLVaoLDCkUZ0N+xoNEqYzEDFRwBVpnGDhlBwvfl3u1svffMxjTVIBnhFRTZ7QvR\/xPEH3dBYKOEWsaAeerhEtl2WV1etJVmi9ohbkv0OZ3NP5\/1A1h2sXlB6MUo\/\/e65SBoJWzof\/JPZ6w483yYAAd4e5u+900LxZvNqzhDzz7yCdEimDEqJX0gJH0PBZUFt4uyoMRVhL3LFpHBTAv7uc\/zfPfTsAkI9eZIbq69bLOExo6+Z\/DzKbRWJhqUkKLsmsZxeXuf2uKzXRn\/z5MWzI30sYBfcZm+tH\/xfSj0S9JWxshu0\/p\/iHdy6x47ea06sgDfU\/V6hBgQ="
+};
+$done({ body: JSON.stringify(body) });
